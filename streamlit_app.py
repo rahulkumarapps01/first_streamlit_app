bf4cd9c140843a2d2a6720cdb2aed01555fef457
rahulkumarapps01 <139931782+rahulkumarapps01@users.noreply.github.com>
2023-07-20 11:05:34 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
 	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 	#streamlit.text(fruityvice_response.json())
 	# take json version and normalize it 
